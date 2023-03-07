c45e24a33e7121f8d0a17230c670b4174e07d61a
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-03-07 07:50:48 +0200

    Add files via upload

---
+++ SQL/Chapter 7.sql
@@ -0,0 +1,237 @@
+--Chapter 7 Table design
+
+/* Declaring a single-column natural key as primary key*/
+
+-- column constraint
+CREATE TABLE natural_key_example (
+    license_id varchar(10) CONSTRAINT license_key PRIMARY KEY,
+    first_name varchar(50),
+    last_name varchar(50)
+);
+
+-- Drop table
+DROP TABLE natural_key_example;
+
+-- table constraint
+CREATE TABLE natural_key_example (
+    license_id varchar(10),
+    first_name varchar(50),
+    last_name varchar(50),
+    CONSTRAINT license_key PRIMARY KEY (license_id)
+);
+
+--primary key key violation
+INSERT INTO natural_key_example (license_id, first_name, last_name)
+VALUES ('T229901', 'Lynn', 'Malero');
+
+INSERT INTO natural_key_example (license_id, first_name, last_name)
+VALUES ('T229901', 'Sam', 'Tracy');
+
+-- Declaring a composite primary key as a natural key
+CREATE TABLE natural_key_composite_example (
+    student_id varchar(10),
+    school_day date,
+    present boolean,
+    CONSTRAINT student_key PRIMARY KEY (student_id, school_day)
+);
+
+/*********/
+
+-- composite primary key violation
+
+INSERT INTO natural_key_composite_example (student_id, school_day, present)
+VALUES(775, '1/22/2017', 'Y');
+
+INSERT INTO natural_key_composite_example (student_id, school_day, present)
+VALUES(775, '1/23/2017', 'Y');
+
+INSERT INTO natural_key_composite_example (student_id, school_day, present)
+VALUES(775, '1/23/2017', 'N');
+
+--Declaring a bigserial column as a surrogate key
+
+CREATE TABLE surrogate_key_example (
+    order_number bigserial,
+    product_name varchar(50),
+    order_date date,
+    CONSTRAINT order_key PRIMARY KEY (order_number)
+);
+
+INSERT INTO surrogate_key_example (product_name, order_date)
+VALUES ('Beachball Polish', '2015-03-17'),
+       ('Wrinkle De-Atomizer', '2017-05-22'),
+       ('Flux Capacitor', '1985-10-26');
+
+SELECT * FROM surrogate_key_example;
+
+--foreign key example
+
+CREATE TABLE licenses (
+    license_id varchar(10),
+    first_name varchar(50),
+    last_name varchar(50),
+    CONSTRAINT licenses_key PRIMARY KEY (license_id)
+);
+
+CREATE TABLE registrations (
+    registration_id varchar(10),
+    registration_date date,
+    license_id varchar(10) REFERENCES licenses (license_id),
+    CONSTRAINT registration_key PRIMARY KEY (registration_id, license_id)
+);
+
+INSERT INTO licenses (license_id, first_name, last_name)
+VALUES ('T229901', 'Lynn', 'Malero');
+
+INSERT INTO registrations (registration_id, registration_date, license_id)
+VALUES ('A203391', '3/17/2017', 'T229901');
+
+INSERT INTO registrations (registration_id, registration_date, license_id)
+VALUES ('A75772', '3/17/2017', 'T000001');
+
+---
+
+-- CHECK constraint examples
+
+CREATE TABLE check_constraint_example (
+    user_id bigserial,
+    user_role varchar(50),
+    salary integer,
+    CONSTRAINT user_id_key PRIMARY KEY (user_id),
+    CONSTRAINT check_role_in_list CHECK (user_role IN('Admin', 'Staff')),
+    CONSTRAINT check_salary_not_zero CHECK (salary > 0)
+);
+
+-- Check violation
+INSERT INTO check_constraint_example (user_role)
+VALUES ('admin');
+
+INSERT INTO check_constraint_example (salary)
+VALUES (0);
+
+---
+
+-- UNIQUE constraint
+
+CREATE TABLE unique_constraint_example (
+    contact_id bigserial CONSTRAINT contact_id_key PRIMARY KEY,
+    first_name varchar(50),
+    last_name varchar(50),
+    email varchar(200),
+    CONSTRAINT email_unique UNIQUE (email)
+);
+
+INSERT INTO unique_constraint_example (first_name, last_name, email)
+VALUES ('Samantha', 'Lee', 'slee@example.org');
+
+INSERT INTO unique_constraint_example (first_name, last_name, email)
+VALUES ('Betty', 'Diaz', 'bdiaz@example.org');
+
+--violates UNIQUE constraint (email)
+INSERT INTO unique_constraint_example (first_name, last_name, email)
+VALUES ('Sasha', 'Lee', 'slee@example.org');
+
+-- NOT NULL constraint
+
+CREATE TABLE not_null_example (
+    student_id bigserial,
+    first_name varchar(50) NOT NULL,
+    last_name varchar(50) NOT NULL,
+    CONSTRAINT student_id_key PRIMARY KEY (student_id)
+);
+
+-- Dropping and adding a primary key and a NOT NULL constraint
+
+-- Drop
+ALTER TABLE not_null_example DROP CONSTRAINT student_id_key;
+
+-- Add
+ALTER TABLE not_null_example ADD CONSTRAINT student_id_key PRIMARY KEY (student_id);
+
+-- Drop
+ALTER TABLE not_null_example ALTER COLUMN first_name DROP NOT NULL;
+
+-- Add
+ALTER TABLE not_null_example ALTER COLUMN first_name SET NOT NULL;
+
+--Importing New York City address data
+
+CREATE TABLE new_york_addresses (
+    longitude numeric(9,6),
+    latitude numeric(9,6),
+    street_number varchar(10),
+    street varchar(32),
+    unit varchar(7),
+    postcode varchar(5),
+    id integer CONSTRAINT new_york_key PRIMARY KEY
+);
+
+COPY new_york_addresses
+FROM 'C:\Users\Marco\Desktop\Postgres SQL\Chapter_07\city_of_new_york.csv'
+WITH (FORMAT CSV, HEADER);
+
+-- Benchmark queries for index performance
+
+EXPLAIN ANALYZE SELECT * FROM new_york_addresses
+WHERE street = 'BROADWAY';
+
+EXPLAIN ANALYZE SELECT * FROM new_york_addresses
+WHERE street = '52 STREET';
+
+EXPLAIN ANALYZE SELECT * FROM new_york_addresses
+WHERE street = 'ZWICKY AVENUE';
+
+-- Creating a B-Tree index on the new_york_addresses table
+
+CREATE INDEX street_idx ON new_york_addresses (street);
+
+--
+CREATE TABLE albums (
+    album_id bigserial,
+    album_catalog_code varchar(100) NOT NULL,
+    album_title text NOT NULL,
+    album_artist text NOT NULL,
+    album_release_date date,
+    album_genre varchar(40),
+    album_description text,
+    CONSTRAINT album_id_key PRIMARY KEY (album_id),
+    CONSTRAINT release_date_check CHECK (album_release_date > '1/1/1925')
+);
+
+CREATE TABLE songs (
+    song_id bigserial,
+    song_title text NOT NULL,
+    song_artist text NOT NULL,
+    album_id bigint REFERENCES albums (album_id),
+    CONSTRAINT song_id_key PRIMARY KEY (song_id)
+);
+
+
+--
+-- Both tables get a primary key using surrogate key id values that are
+-- auto-generated via serial data types.
+
+-- The songs table references albums via a foreign key constraint.
+
+-- In both tables, the title and artist columns cannot be empty, which
+-- is specified via a NOT NULL constraint. We assume that every album and
+-- song should atleast have that information.
+
+-- In albums, the album_release_date column has a CHECK constraint
+-- because it would be unlikely for us to own an LP made before 1925.
+
+--2) We could consider the album_catalog_code.
+-- - It is going to be unique across all albums released by all companies.
+
+--3)
+/* Primary key columns get indexes by default, but we should add an index
+to the album_id foreign key column in the songs table because we'll use
+it in table joins. It's likely that we'll query these tables to search
+by titles and artists, so those columns in both tables should get indexes
+too. The album_release_date in albums also is a candidate if we expect
+to perform many queries that include date ranges.
+*/
+
+
+
+
