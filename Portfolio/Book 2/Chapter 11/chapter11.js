159c32666da7aec6f2663352cce136f1daff1784
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-03-18 18:11:43 +0200

    Add files via upload

---
+++ Portfolio/Book 2/Chapter 11/chapter11.js
@@ -0,0 +1,5 @@
+var people = ["Kareem", "Fatma", "Sumaya", "Mr. Hobson", "Ms. Young", "Mrs. O' Doherty"];
+var otherPeople = ["Emilie", "Mommy", "Dad", "Kathy"];
+
+document.getElementById("peopleIKnow").innerHTML = people.valueOf();
+
