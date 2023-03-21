ad0530424c12b619177139bc61d2911b9289bac4
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-03-21 11:28:15 +0200

    Add files via upload

---
+++ SQL/Chapter_09.sql
@@ -0,0 +1,81 @@
+
+-- Chapter 9 
+--------------------------------------------------------------
+
+-- Listing 9-1: Importing the FSIS Meat, Poultry, and Egg Inspection Directory
+
+
+CREATE TABLE meat_poultry_egg_inspect (
+    est_number varchar(50) CONSTRAINT est_number_key PRIMARY KEY,
+    company varchar(100),
+    street varchar(100),
+    city varchar(30),
+    st varchar(2),
+    zip varchar(5),
+    phone varchar(14),
+    grant_date date,
+    activities text,
+    dbas text
+);
+
+COPY meat_poultry_egg_inspect
+FROM 'C:\YourDirectory\MPI_Directory_by_Establishment_Name.csv'
+WITH (FORMAT CSV, HEADER, DELIMITER ',');
+
+
+-- Listing 9-2: Finding multiple companies at the same address
+SELECT company,
+       street,
+       city,
+       st,
+       count(*) AS address_count
+FROM meat_poultry_egg_inspect
+GROUP BY company, street, city, st
+HAVING count(*) > 1
+ORDER BY company, street, city, st;
+
+-- Listing 9-7: Filtering with length() to find short zip values
+
+SELECT st,
+       count(*) AS st_count
+FROM meat_poultry_egg_inspect
+WHERE length(zip) < 5
+GROUP BY st
+ORDER BY st ASC;
+
+-- Listing 9-8: Backing up a table
+
+CREATE TABLE meat_poultry_egg_inspect_backup AS
+SELECT * FROM meat_poultry_egg_inspect;
+
+-- Check number of records:
+SELECT 
+    (SELECT count(*) FROM meat_poultry_egg_inspect) AS original,
+    (SELECT count(*) FROM meat_poultry_egg_inspect_backup) AS backup;
+
+-- Listing 9-11: Updating the st column for three establishments
+
+UPDATE meat_poultry_egg_inspect
+SET st = 'MN'
+WHERE est_number = 'V18677A';
+
+UPDATE meat_poultry_egg_inspect
+SET st = 'AL'
+WHERE est_number = 'M45319+P45319';
+
+UPDATE meat_poultry_egg_inspect
+SET st = 'WI'
+WHERE est_number = 'M263A+P263A+V263A';
+
+-- Listing 9-12: Restoring original st column values
+
+-- Restoring from the column backup
+UPDATE meat_poultry_egg_inspect
+SET st = st_copy;
+
+-- Restoring from the table backup
+UPDATE meat_poultry_egg_inspect original
+SET st = backup.st
+FROM meat_poultry_egg_inspect_backup backup
+WHERE original.est_number = backup.est_number; 
+
